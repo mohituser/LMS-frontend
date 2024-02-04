0d765265f7dc5b448570a0b203c94f635989bc38
mohituser <122739741+mohituser@users.noreply.github.com>
2024-02-05 02:22:56 +0530

    Update CourseDescription.jsx

--- src/Pages/Course/CourseDescription.jsx
+++ src/Pages/Course/CourseDescription.jsx
@@ -53,7 +53,7 @@ function CourseDescription() {
                                     Watch lectures
                                 </button>
                                 ) : (
-                                    <button onClick={() => navigate("/checkout")} className="bg-yellow-600 text-xl rounded-md font-bold px-5 py-3 w-full hover:bg-yellow-500 transition-all ease-in-out duration-300">
+                                    <button onClick={() => navigate("/login")} className="bg-yellow-600 text-xl rounded-md font-bold px-5 py-3 w-full hover:bg-yellow-500 transition-all ease-in-out duration-300">
                                         Subscribe
                                     </button>
                                 )
